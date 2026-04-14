699bd91fa5f1ca3c1583fcf355a86bf2a59be4d6
NelsonJ <nelsonjcelisc@gmail.com>
2026-04-13 20:55:16 -0500

    reiniciar servicios tras apagones

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+#!/bin/bash
 # Ir a la carpeta del proyecto
 cd /mnt/data/runtime/analitica_redes_sociales
+# Si ya está arriba, Docker no hace nada. Si está abajo, lo levanta.
+/usr/bin/docker compose up -d
+sleep 5
 # Activar el venv usando la ruta completa
 source /mnt/data/runtime/analitica_redes_sociales/venv/bin/activate
 # Ejecutar el script
